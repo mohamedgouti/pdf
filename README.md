1070db9c0102dbb772ce39deaeb90d5de4d50d43
Gouti <mohamed.gouti@esprit.tn>
2018-12-24 11:23:29 +0100

    pdfsnappy

---
+++ README.md
@@ -0,0 +1,4 @@
+.checkout
+=========
+
+A Symfony project created on December 23, 2018, 12:07 pm.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        $snappy = $this->get('knp_snappy.pdf');
+
+        $html = $this->renderView('default/index.html.twig', array(
+            'title' => 'Hello World !'
+        ));
+
+        $filename = 'myFirstSnappyPDF';
+
+        return new Response(
+            $snappy->getOutputFromHtml($html),
+            200,
+            array(
+                'Content-Type'          => 'application/pdf',
+                'Content-Disposition'   => 'inline; filename="'.$filename.'.pdf"'
+            )
+        );
+    }
+}
